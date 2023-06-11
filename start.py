b87d88c763d08fcd3743ceaf821b6b65b2551c3c
Sebastian Graef <segraef@microsoft.com>
2023-06-11 20:28:09 +1000

    changed voice and added error handling

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ import openai
 speech_key = os.environ.get('cognitive_services_speech_key')
 service_region, endpoint = "australiaeast", "https://australiaeast.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-speech_config.speech_synthesis_voice_name = "en-US-AriaNeural"
+speech_config.speech_synthesis_voice_name = "en-US-AshleyNeural" # https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support?tabs=tts
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
@@ -16,16 +16,28 @@ openai.api_base = os.environ.get('openai_api_base')
 openai.api_type = 'azure'
 openai.api_version = '2023-05-15'
 
-def openai_generate_response(input_text):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=input_text,
-        max_tokens=100
-    )
-    return response.choices[0].text.strip()
+# Define a function to generate a response using Azure OpenAI
+def openai_generate_response(prompt):
+    try:
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=1000,
+            temperature=0.5,
+        )
+        return response.choices[0].text.strip()
+    except openai.error.InvalidRequestError as e:
+        print(f"Invalid request error: {e}")
+    except openai.error.AuthenticationError as e:
+        print(f"Authentication error: {e}")
+    except openai.error.APIConnectionError as e:
+        print(f"API connection error: {e}")
+    except openai.error.OpenAIError as e:
+        print(f"OpenAI error: {e}")
 
 print("Say something...")
 
+# Recognize speech from an audio input stream
 result_stt = speech_recognizer.recognize_once()
 
 # Check speech recognition result
